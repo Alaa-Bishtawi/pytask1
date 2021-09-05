a4f73b5cbc90c24d20f04f6fcd397364f3e255db
Alaa-Bishtawi <bish.h.alaa@gmail.com>
2021-09-05 15:01:35 +0300

    Unit Testing

---
+++ testing.py
@@ -0,0 +1,31 @@
+import unittest
+from models import Link
+
+import  string
+
+class TestLInk(unittest.TestCase):
+    def test_boolean(self):
+        OrginalUrl = "https://stackoverflow.com/questions/23944657/typeerror-method-takes-1-positional-argument-but-2-were-given"
+        link = Link()
+        b = link.isValidURL(OrginalUrl)
+        a = True
+
+        self.assertEqual(a, b)
+
+
+
+    def test_string(self):
+        OrginalUrl = "https://stackoverflow.com/questions/23944657/typeerror-method-takes-1-positional-argument-but-2-were-given"
+        ShortUrl = "http://127.0.0.1:5000/616564bWiuut"
+        a = len(ShortUrl)
+        ShortUrl = ShortUrl[0:a - 6]
+
+        link = Link()
+        GeneratedShortendUrl = link.ShortenUrl(OrginalUrl)
+        b = len(GeneratedShortendUrl)
+        GeneratedShortendUrl = link.server_url + GeneratedShortendUrl[0:b-6]
+        self.assertEqual(ShortUrl, GeneratedShortendUrl)
+
+
+if __name__ == '__main__':
+    unittest.main()
